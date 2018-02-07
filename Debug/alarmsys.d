62f53af13c97813c8e743bd4e397579e90fba8d0
loethansel <ralf@pandel.de>
2018-02-07 11:59:41 +0100

    first own changes

--- Debug/alarmsys.d
+++ Debug/alarmsys.d
@@ -9,25 +9,7 @@ alarmsys.o: ../alarmsys.cpp ../blacklib/BlackLib.h \
  ../blacklib/BlackThread/BlackThread.h \
  ../blacklib/BlackMutex/BlackMutex.h \
  ../blacklib/BlackDirectory/BlackDirectory.h \
- ../blacklib/BlackTime/BlackTime.h ../blacklib/examples/example_GPIO.h \
- ../blacklib/examples/../BlackGPIO/BlackGPIO.h \
- ../blacklib/examples/example_ADC.h \
- ../blacklib/examples/../BlackADC/BlackADC.h \
- ../blacklib/examples/example_PWM.h \
- ../blacklib/examples/../BlackPWM/BlackPWM.h \
- ../blacklib/examples/example_SPI.h \
- ../blacklib/examples/../BlackSPI/BlackSPI.h \
- ../blacklib/examples/example_UART.h \
- ../blacklib/examples/../BlackUART/BlackUART.h \
- ../blacklib/examples/example_I2C.h \
- ../blacklib/examples/../BlackI2C/BlackI2C.h \
- ../blacklib/examples/example_directory.h \
- ../blacklib/examples/../BlackDirectory/BlackDirectory.h \
- ../blacklib/examples/example_threadAndMutex.h \
- ../blacklib/examples/../BlackThread/BlackThread.h \
- ../blacklib/examples/../BlackMutex/BlackMutex.h \
- ../blacklib/examples/example_time.h \
- ../blacklib/examples/../BlackTime/BlackTime.h
+ ../blacklib/BlackTime/BlackTime.h ../blacklib/BlackUART/BlackUART.h
 
 ../blacklib/BlackLib.h:
 
@@ -69,40 +51,4 @@ alarmsys.o: ../alarmsys.cpp ../blacklib/BlackLib.h \
 
 ../blacklib/BlackTime/BlackTime.h:
 
-../blacklib/examples/example_GPIO.h:
-
-../blacklib/examples/../BlackGPIO/BlackGPIO.h:
-
-../blacklib/examples/example_ADC.h:
-
-../blacklib/examples/../BlackADC/BlackADC.h:
-
-../blacklib/examples/example_PWM.h:
-
-../blacklib/examples/../BlackPWM/BlackPWM.h:
-
-../blacklib/examples/example_SPI.h:
-
-../blacklib/examples/../BlackSPI/BlackSPI.h:
-
-../blacklib/examples/example_UART.h:
-
-../blacklib/examples/../BlackUART/BlackUART.h:
-
-../blacklib/examples/example_I2C.h:
-
-../blacklib/examples/../BlackI2C/BlackI2C.h:
-
-../blacklib/examples/example_directory.h:
-
-../blacklib/examples/../BlackDirectory/BlackDirectory.h:
-
-../blacklib/examples/example_threadAndMutex.h:
-
-../blacklib/examples/../BlackThread/BlackThread.h:
-
-../blacklib/examples/../BlackMutex/BlackMutex.h:
-
-../blacklib/examples/example_time.h:
-
-../blacklib/examples/../BlackTime/BlackTime.h:
+../blacklib/BlackUART/BlackUART.h:
--- alarmsys.cpp
+++ alarmsys.cpp
@@ -26,10 +26,12 @@
  ####################################################################################
 
  */
-
-
 #include "blacklib/BlackLib.h"
-
+#include "blacklib/BlackUART/BlackUART.h"
+#include <string>
+#include <iostream>
+#include <iomanip>
+/*
 #include "blacklib/examples/example_GPIO.h"
 #include "blacklib/examples/example_ADC.h"
 #include "blacklib/examples/example_PWM.h"
@@ -39,18 +41,56 @@
 #include "blacklib/examples/example_directory.h"
 #include "blacklib/examples/example_threadAndMutex.h"
 #include "blacklib/examples/example_time.h"
+*/
 
+void Init_UART()
+{
+    /*
+     * This part of the example code is about BlackUART class, write/read buffer and write/read
+     * string initialization.
+     *
+     * 1) BlackUART(enum uartName uart);
+     * 2) BlackUART(enum uartName uart, struct BlackUartProperties uartProperties);
+     * 3) BlackUART(enum uartName uart, enum baudRate uartBaud, enum parity uartParity,
+     *                                  enum stopBits uartStopBits, enum characterSize uartCharSize);
+     */
+
+    BlackLib::BlackUART gsm( BlackLib::UART1,
+                             BlackLib::Baud9600,
+                             BlackLib::ParityEven,
+                             BlackLib::StopOne,
+                             BlackLib::Char8);
+
+
+    bool isOpened = gsm.open( BlackLib::ReadWrite | BlackLib::NonBlock );
+
+    if(!isOpened) {
+        std::cout << "UART DEVICE CAN\'T OPEN" << std::endl;
+        exit(1);
+    }
+    std::cout << std::endl;
+    std::cout << "Device Path     : " << gsm.getPortName() << std::endl;
+    std::cout << "Read Buf. Size  : " << gsm.getReadBufferSize() << std::endl;
+    std::cout << "BaudRate In/Out : " << gsm.getBaudRate( BlackLib::input) << "/"
+                                      << gsm.getBaudRate( BlackLib::output) << std::endl;
+    std::cout << "Character Size  : " << gsm.getCharacterSize() << std::endl;
+    std::cout << "Stop Bit Size   : " << gsm.getStopBits() << std::endl;
+    std::cout << "Parity          : " << gsm.getParity() << std::endl << std::endl;
+}
 
 
 
 int main()
 {
 
+    Init_UART();
+
+
 //    example_GPIO();
 //    example_ADC();
 //    example_PWM();
 //    example_SPI();
-    example_UART();
+//    example_UART();
 //    example_I2C();
 //    example_directory();
 //    example_threadAndMutex();
